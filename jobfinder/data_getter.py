97c0d62831a30d3b7a4e0e45d0f066411f104949
Gautam <yashgautam2003@gmail.com>
2024-08-25 23:49:37 +0530

    Database added with job links

--- jobfinder/data_getter.py
+++ jobfinder/data_getter.py
@@ -13,11 +13,11 @@ def selenium_init():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     service = Service(
-        r"C:\Users\Asus\Downloads\chromedriver.exe")  # Update with your ChromeDriver path
+        r"C:\Users\yashg\Desktop\Projects\chromedriver-win64\chromedriver.exe")  # Update with your ChromeDriver path
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
 
-def scrape_jobs(driver,url):
+def scrape_naukri_jobs(driver,url):
     driver.get(url)
     jobs = []
     time.sleep(5)
@@ -34,10 +34,14 @@ def scrape_jobs(driver,url):
         job_details_element = job_listing.find_element(By.CLASS_NAME, 'row4')
         job_details = job_details_element.text.strip() if job_details_element else 'N/A'
 
+        link_element = job_listing.find_element(By.TAG_NAME, 'a')
+        job_link = link_element.get_attribute('href') if link_element else 'N/A'
+
         job = {
             'Job Title': job_title,
             'Company Name': company_name,
-            'Job Details': job_details
+            'Job Details': job_details,
+            'Job Link': job_link
         }
         jobs.append(job)
     # print(jobs)
@@ -51,8 +55,7 @@ def scrape_jobs(driver,url):
     return df_jobs
     # return jobs
 
-def jobs(profile):
-    driver = selenium_init()
+def get_naukri_jobs(driver,profile):
     if(profile=="Software Developer"):
         url = 'https://www.naukri.com/software-developer-jobs'
     elif (profile=="UI UX"):
@@ -63,17 +66,82 @@ def jobs(profile):
         url = 'https://www.naukri.com/app-developer-jobs'
     elif (profile == "AI ML"):
         url = 'https://www.naukri.com/machine-learning-jobs'
-    jobs = scrape_jobs(driver, url)
-    print(jobs)
+    # url="https://www.naukri.com/software-developer-jobs"
+    # print(driver)
+    naukri_jobs = scrape_naukri_jobs(driver, url)
+    # print(naukri_jobs)
+    return naukri_jobs
+
+
+def get_indeed_jobs(driver,profile):
+    print(profile)
+    if(profile=="Software Developer"):
+            url = 'https://in.indeed.com/jobs?q=software+developer'
+    elif (profile=="UI UX"):
+        url = 'https://in.indeed.com/jobs?q=ui-ux-jobs'
+    elif (profile == "Graphic Designer"):
+        url = 'https://in.indeed.com/jobs?q=graphic-designer-jobs'
+    elif (profile == "App Developer"):
+        url = 'https://in.indeed.com/jobs?q=app-developer-jobs'
+    elif (profile == "AI ML"):
+        url = 'https://in.indeed.com/jobs?q=machine-learning-jobs'
+    naukri_jobs = scrape_jobs(driver, url)
+    print(naukri_jobs)
+
+def scrape_indeed_jobs(driver,url):
+    driver.get(url)
+    jobs = []
+    time.sleep(5)
+    # Wait for the job listings to load
+    job_tuple_element = driver.find_elements(By.CLASS_NAME, 'sjw__tuple')
+    # print(job_tuple_element)
+    for job_listing in job_tuple_element:
+        job_title_element = job_listing.find_element(By.CLASS_NAME, 'row1')
+        job_title = job_title_element.text.strip() if job_title_element else 'N/A'
+
+        company_name_element = job_listing.find_element(By.CLASS_NAME, 'row2')
+        company_name = company_name_element.text.strip() if company_name_element else 'N/A'
+
+        job_details_element = job_listing.find_element(By.CLASS_NAME, 'row4')
+        job_details = job_details_element.text.strip() if job_details_element else 'N/A'
+
+        link_element = job_listing.find_element(By.TAG_NAME, 'a')
+        job_link = link_element.get_attribute('href') if link_element else 'N/A'
+
+        job = {
+            'Job Title': job_title,
+            'Company Name': company_name,
+            'Job Details': job_details,
+            'Job Link': job_link
+        }
+        print(job)
+        jobs.append(job)
+    # print(jobs)
+    df_jobs = pd.DataFrame(jobs)
+    name_list=df_jobs.iloc[:,1]
+    for i in range(0,len(name_list)-1):
+        a=name_list[i].split("\n")
+        name_list[i]=a[0]
+    df_jobs["Company Name"]=name_list
+    driver.quit()
+    return df_jobs
+    # return jobs
+
+def jobs(profile):
     JobListing.objects.filter(job_type=profile).delete()
+    driver = selenium_init()
+    naukri_jobs=get_naukri_jobs(driver,profile)
+    # driver = selenium_init()
+    # indeed_jobs=get_indeed_jobs(profile, driver)
     job_listings = [
         JobListing(
             job_title=row['Job Title'],
             job_type=profile,
             company_name=row['Company Name'],
-            job_details=row['Job Details']
+            job_details=row['Job Details'],
+            job_link=row['Job Link']
         )
-        for index, row in jobs.iterrows()
+        for index, row in naukri_jobs.iterrows()
     ]
 
     JobListing.objects.bulk_create(job_listings)
--- jobfinder/models.py
+++ jobfinder/models.py
@@ -6,6 +6,7 @@ class JobListing(models.Model):
     job_type=models.CharField(max_length=255 , default="Nothing Here")
     company_name = models.CharField(max_length=255)
     job_details = models.TextField()
+    job_link = models.CharField(max_length=255, default ="None")
 
     def __str__(self):
         return self.job_title
